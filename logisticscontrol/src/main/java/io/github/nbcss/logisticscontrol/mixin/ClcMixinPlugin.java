ea0023876dad9bd795f636d30f09df3a5176ec15
nbcss <nbcss233@gmail.com>
2026-08-15 20:21:25 +1000

    added fluid logistics compat for filter link

--- logisticscontrol/src/main/java/io/github/nbcss/logisticscontrol/mixin/ClcMixinPlugin.java
+++ logisticscontrol/src/main/java/io/github/nbcss/logisticscontrol/mixin/ClcMixinPlugin.java
@@ -12,8 +12,11 @@ public class ClcMixinPlugin implements IMixinConfigPlugin {
 
     private static final String DEPLOYER_ORDER_PACKET_MIXIN =
         "io.github.nbcss.logisticscontrol.mixin.GenericOrderRequestPacketMixin";
+    private static final String FLUID_REPACKAGER_MIXIN =
+        "io.github.nbcss.logisticscontrol.mixin.FluidRepackagerFilterMixin";
 
     private static final boolean DEPLOYER_PRESENT = isModPresent("deployer");
+    private static final boolean FLUIDLOGISTICS_PRESENT = isModPresent("fluidlogistics");
 
     private static boolean isModPresent(String modId) {
         try {
@@ -26,6 +29,7 @@ public class ClcMixinPlugin implements IMixinConfigPlugin {
     @Override
     public boolean shouldApplyMixin(String targetClassName, String mixinClassName) {
         if (DEPLOYER_ORDER_PACKET_MIXIN.equals(mixinClassName)) return DEPLOYER_PRESENT;
+        if (FLUID_REPACKAGER_MIXIN.equals(mixinClassName)) return FLUIDLOGISTICS_PRESENT;
         return true;
     }
 
